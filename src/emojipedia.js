630e02feae5fb1ee5e7f6a05b36dcf9174c29af1
Aniket Pal <aniketindian8@gmail.com>
2020-12-03 16:34:19 +0530

    Initialized

---
+++ src/emojipedia.js
@@ -0,0 +1,90 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🐼",
+    name: "Panda",
+    meaning:
+      "“I'm going to a Cro concert! Pandas come from China, feed on bamboo and are threatened with extinction. You find something totally cute or as an expression of affection within friendships."
+  },
+  {
+    id: 2,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning:
+      "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things."
+  },
+  {
+    id: 3,
+    emoji: "🌚",
+    name: "New Moon With Face",
+    meaning:
+      "New moon with face. Is usually perceived as creepy and eerie. Can also symbolize the man in the moon."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text."
+  },
+  {
+    id: 5,
+    emoji: "😃",
+    name: "Smiling Face With Open Mouth",
+    meaning:
+      "Typical smiley face with open mouth and oval eyes. Is in a positive mood, shows its teeth and laughs cheerfully. Expresses enthusiasm: from a cheerful greeting to boundless joy."
+  },
+  {
+    id: 6,
+    emoji: "😄",
+    name: "Smiling Face With Open Mouth And Smiling Eyes",
+    meaning:
+      "Smiley's mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm."
+  },
+  {
+    id: 7,
+    emoji: "😆",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.. Means the same as “XD“ and “X“ as a text-based emoticon."
+  },
+  {
+    id: 8,
+    emoji: "😂",
+    name: " Face With Tears of Joy",
+    meaning:
+      "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015."
+  },
+  {
+    id: 9,
+    emoji: "😇",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 10,
+    emoji: "🙂",
+    name: " Slightly Smiling Face",
+    meaning:
+      "A light smile means you are satisfied with yourself and the world. Can make a statement sound friendlier than it might have been meant. Or have a sarcastic meaning when a smile does not actually fit the content."
+  },
+  {
+    id: 11,
+    emoji: "😚",
+    name: "Kissing Face With Closed Eyes",
+    meaning:
+      "Cute face with closed eyes and rosy cheeks. As a thank you for a tip or a favor. Loving kiss to the closest friends, family or darling."
+  },
+  {
+    id: 12,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+
+  
+];
+
+export default emojipedia;
